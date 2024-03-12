486bd1d37c564950f23f50b4a85086fd49444e8b
Kyle Shapka <Kyle.Shapka@gov.bc.ca>
2024-03-12 15:59:51 -0700

    removed accidental newline in start.php

--- src/start.php
+++ src/start.php
@@ -5,6 +5,5 @@ use Bcgov\NaadConnector\DestinationClient;
 use Bcgov\NaadConnector\NaadSocketClient;
 
 $destinationClient = new DestinationClient($argv[3], $argv[4], $argv[5]);
-
 $connector = new NaadSocketClient($argv[1], $argv[2], $destinationClient);
 return $connector->connect();
