a50114a3d4cb688875294650655c9db66069bbd1
robrien-bcgov <richard.obrien@gov.bc.ca>
2024-12-02 15:25:52 -0800

    descw-2793 got initial linting set up and test lint errors fixed.

--- tests/NaadSocketClientTest.php
+++ tests/NaadSocketClientTest.php
@@ -1,4 +1,7 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
 use PHPUnit\Framework\Attributes\{
     Test,
     CoversClass,
@@ -19,27 +22,37 @@ use Bcgov\NaadConnector\Entity\Alert;
 
 #[CoversClass('Bcgov\NaadConnector\NaadSocketClient')]
 #[UsesClass('Bcgov\NaadConnector\Entity\Alert')]
-final class NaadSocketClientTest extends TestCase {
+/**
+ * NaadSocketClientTest Class for testing NaadSocketClient.
+ * Uses the \Entity\Alert class.
+ *
+ * @category Client
+ * @package  NaadConnector
+ * @author   Michael Haswell <Michael.Haswell@gov.bc.ca>
+ * @license  https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link     https://alerts.pelmorex.com/
+ */
+final class NaadSocketClientTest extends TestCase
+{
 
     const XML_TEST_FILE_LOCATION = './tests/Socket/';
 
     #[Test]
     #[DataProvider('handleResponseProvider')]
-    public function testHandleResponse(array $xmlResponses) {
-        $database = $this->createStub( Database::class );
-        $destinationClient = $this->createStub( DestinationClient::class );
-        $logger = $this->createStub(CustomLogger::class);
-        $repositoryClient = $this->createStub( NaadRepositoryClient::class );
-
-        $client = new NaadSocketClient( 'test-naad', $destinationClient, $logger, $database, $repositoryClient );
-        libxml_use_internal_errors(true);
-        foreach($xmlResponses as $response) {
-            $result = $client->handleResponse(file_get_contents( self::XML_TEST_FILE_LOCATION . $response['location'] ));
-            $this->assertEquals($response['expected'], $result);
-        }
-    }
-
-    public static function handleResponseProvider() {
+    /**
+     * Tests the handleResponse method of the NaadSocketClient class.
+     *
+     * This method tests the handleResponse method with various XML responses.
+     * It checks if the method correctly handles different types of responses,
+     * including single part responses, multi-part responses,
+     * and heartbeat responses.
+     *
+     * @dataProvider handleResponseProvider
+     *
+     * @return array The test responses
+     */
+    public static function handleResponseProvider()
+    {
         return [
             'Success - single part'                => [
                 [
@@ -129,81 +142,171 @@ final class NaadSocketClientTest extends TestCase {
     }
 
     #[Test]
-    public function testHandleResponseMissedAlerts() {
-        $database = $this->createStub( Database::class );
+    /**
+     * Tests the handleResponse method of the NaadSocketClient class when there are
+     * missed alerts.
+     *
+     * This method tests the handleResponse method with a heartbeat response
+     * that contains missed alerts.
+     * It checks if the method correctly handles the missed alerts by fetching them
+     * from the repository.
+     *
+     * @return void
+     */
+    public function testHandleResponseMissedAlerts()
+    {
+        $database = $this->createStub(Database::class);
         $database->method('getAlertsById')->willReturn([]);
 
-        $destinationClient = $this->createStub( DestinationClient::class );
-        $logger = $this->createStub( CustomLogger::class );
+        $destinationClient = $this->createStub(DestinationClient::class);
+        $logger = $this->createStub(CustomLogger::class);
 
-        $repositoryClient = $this->createMock( NaadRepositoryClient::class );
+        $repositoryClient = $this->createMock(NaadRepositoryClient::class);
         // Should fetch 10 times because all 10 heartbeat reference ids are new.
         $repositoryClient
             ->expects($this->exactly(10))
             ->method('fetchAlert')
-            ->willReturn(file_get_contents( self::XML_TEST_FILE_LOCATION . 'complete-alert.xml' ));
+            ->willReturn( file_get_contents( self::XML_TEST_FILE_LOCATION . 'complete-alert.xml'));
 
-        $client = new NaadSocketClient( 'test-naad', $destinationClient, $logger, $database, $repositoryClient );
+        $client = new NaadSocketClient(
+            'test-naad',
+            $destinationClient,
+            $logger,
+            $database,
+            $repositoryClient
+        );
         libxml_use_internal_errors(true);
 
-        $result = $client->handleResponse(file_get_contents( self::XML_TEST_FILE_LOCATION . 'heartbeat.xml' ));
+        $result = $client->handleResponse(
+            file_get_contents(self::XML_TEST_FILE_LOCATION . 'heartbeat.xml')
+        );
         $this->assertEquals(true, $result);
     }
 
     #[Test]
-    public function testHandleResponseExistingMissedAlert() {
-        $alertXml = file_get_contents(self::XML_TEST_FILE_LOCATION . 'complete-alert.xml');
 
-        $database = $this->createStub( Database::class );
+    /**
+     * Tests the handleResponse method of the NaadSocketClient class when there are
+     * existing missed alerts.
+     *
+     * This method tests the handleResponse method with a heartbeat response
+     * that contains missed alerts, where one of the alerts is already
+     * in the database.
+     * It checks if the method correctly handles the missed alerts by fetching them
+     * from the repository.
+     *
+     * @return void
+     */
+    public function testHandleResponseExistingMissedAlert()
+    {
+        $alertXml = file_get_contents(
+            self::XML_TEST_FILE_LOCATION . 'complete-alert.xml'
+        );
+
+        $database = $this->createStub(Database::class);
         $database
             ->method('getAlertsById')
             ->willReturn([Alert::fromXml(new SimpleXMLElement($alertXml))]);
 
-        $destinationClient = $this->createStub( DestinationClient::class );
-        $logger = $this->createStub( CustomLogger::class );
+        $destinationClient = $this->createStub(DestinationClient::class);
+        $logger = $this->createStub(CustomLogger::class);
 
-        $repositoryClient = $this->createMock( NaadRepositoryClient::class );
+        $repositoryClient = $this->createMock(NaadRepositoryClient::class);
         // Should only fetch 9 times because one was already in the database.
         $repositoryClient
             ->expects($this->exactly(9))
             ->method('fetchAlert');
 
-        $client = new NaadSocketClient( 'test-naad', $destinationClient, $logger, $database, $repositoryClient );
+        $client = new NaadSocketClient(
+            'test-naad',
+            $destinationClient,
+            $logger,
+            $database,
+            $repositoryClient
+        );
+
         libxml_use_internal_errors(true);
 
-        $result = $client->handleResponse(file_get_contents( self::XML_TEST_FILE_LOCATION . 'heartbeat.xml' ));
+        $result = $client->handleResponse(
+            file_get_contents(self::XML_TEST_FILE_LOCATION . 'heartbeat.xml')
+        );
         $this->assertEquals(true, $result);
     }
 
     #[Test]
-    public function testHandleResponseDatabaseException() {
-        $database = $this->createStub( Database::class );
+    /**
+     * Tests the handleResponse method of the NaadSocketClient class when
+     * a database  exception occurs.
+     *
+     * This method tests the handleResponse method with a valid XML response, but
+     * simulates a database exception when trying to insert the
+     * alert into the database.
+     * It checks if the method correctly handles the database exception and throws
+     * an exception.
+     *
+     * @return void
+     */
+    public function testHandleResponseDatabaseException()
+    {
+        $database = $this->createStub(Database::class);
         $database->method('insertAlert')->willThrowException(new Exception());
-        $destinationClient = $this->createStub( DestinationClient::class );
-        $logger = $this->createStub( CustomLogger::class );
-        $repositoryClient = $this->createStub( NaadRepositoryClient::class );
+        $destinationClient = $this->createStub(DestinationClient::class);
+        $logger = $this->createStub(CustomLogger::class);
+        $repositoryClient = $this->createStub(NaadRepositoryClient::class);
 
-        $client = new NaadSocketClient( 'test-naad', $destinationClient, $logger, $database, $repositoryClient );
+        $client = new NaadSocketClient(
+            'test-naad',
+            $destinationClient,
+            $logger,
+            $database,
+            $repositoryClient
+        );
+
         libxml_use_internal_errors(true);
 
         $this->expectException(Exception::class);
 
-        $client->handleResponse(file_get_contents( self::XML_TEST_FILE_LOCATION . 'complete-alert.xml' ));
+
+        $client->handleResponse(
+            file_get_contents(self::XML_TEST_FILE_LOCATION . 'complete-alert.xml')
+        );
     }
 
     #[Test]
-    public function testHandleResponseMissingIdentifier() {
+    /**
+     * Tests the handleResponse method of the NaadSocketClient class when the XML
+     * response is missing the identifier field.
+     *
+     * This method tests the handleResponse method with an XML response
+     * that does not contain the required identifier field.
+     * It checks if the method correctly handles the missing identifier
+     * field and throws an exception.
+     *
+     * @return void
+     */
+    public function testHandleResponseMissingIdentifier()
+    {
         $database = $this->createStub(Database::class);
         $destinationClient = $this->createStub(DestinationClient::class);
         $logger = $this->createStub(CustomLogger::class);
         $repositoryClient = $this->createStub(NaadRepositoryClient::class);
 
-        $client = new NaadSocketClient('test-naad', $destinationClient, $logger, $database, $repositoryClient);
+        $client = new NaadSocketClient(
+            'test-naad',
+            $destinationClient,
+            $logger,
+            $database,
+            $repositoryClient
+        );
         libxml_use_internal_errors(true);
 
         $this->expectException(Exception::class);
-        $this->expectExceptionMessage('Invalid XML: The "identifier" field is required.');
+        $this->expectExceptionMessage(
+            'Invalid XML: The "identifier" field is required.'
+        );
 
-        $client->handleResponse(file_get_contents(self::XML_TEST_FILE_LOCATION . 'empty-identifier.xml'));
+        $client->handleResponse(
+            file_get_contents(self::XML_TEST_FILE_LOCATION . 'empty-identifier.xml')
+        );
     }
 }
