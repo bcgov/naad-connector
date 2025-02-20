ca9b10dbb61a96ce0ff7bcb94c852a976d62f902
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-02-20 15:25:24 -0800

    remove debugging from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# Print the current user
-echo "Running as user: $(whoami) UID: $(id -u) GID: $(id -g)"
 # All environment variables will be extracted from the .env inside start.php using getenv().
 /usr/local/bin/php /app/src/start.php
