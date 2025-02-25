10181969fe1e70b531ea3f32b7a65e5f57a95cb1
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-02-25 14:50:33 -0800

    descw-2753 restore entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-set -e
-set -x
-if [-f /vault/secrets/naad]; then
-    source /vault/secrets/naad
-fi
+
 # All environment variables will be extracted from the .env inside start.php using getenv().
 /usr/local/bin/php /app/src/start.php
