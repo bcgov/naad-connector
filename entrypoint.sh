6c5886ad83a5c482ab2a06dbbea2ba35e1f4ec7b
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-02-25 14:20:10 -0800

    DESCW-2863 entrypoint changes to force vault secrets to be exported

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-
+set -e
+set -x
+if [-f /vault/secrets/naad]; then
+    source /vault/secrets/naad
+fi
 # All environment variables will be extracted from the .env inside start.php using getenv().
 /usr/local/bin/php /app/src/start.php
