fecb9c9d24896c19969886da3815184026736918
Michael Haswell <Michael.Haswell@gov.bc.ca>
2025-02-26 08:50:40 -0800

    Add instructions for deploying to Kubernetes to README Improved logging in e2e/start.php script

--- tests/e2e/start.php
+++ tests/e2e/start.php
@@ -3,33 +3,27 @@
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Monolog\Processor\PsrLogMessageProcessor;
 
 require_once 'vendor/autoload.php';
 
-/**
- * This script creates a socket to allow manual testing of the
- * NaadConnector application.
- * 
- * Usage: `php src/start_socket_server.php <path to alert xml file>`
- * 
- * A socket will be created on 0.0.0.0:8080 which the application
- * can be configured to connect to instead of the real NAAD socket.
- * This socket will then immediately send the xml provided as a
- * message to be consumed by the application.
- */
 $socket = new React\Socket\SocketServer('0.0.0.0:8080');
+$logger = new Logger(
+    'e2e',
+    [new StreamHandler('php://stdout')],
+    [new PsrLogMessageProcessor(),]
+);
+$logger->info(
+    'Starting socket server at {address}.',
+    ['address' => $socket->getAddress()]
+);
+
 $path = 'tests/data/';
-
-$logger = new Logger('e2e', [new StreamHandler('php://stdout')]);
-$logger->info('Starting socket server.');
-$logger->info($socket->getAddress());
-
 $file = $path . 'earthquake-1.xml';
-$file2 = $path . 'earthquake-2.xml';
 $socket->on(
     'connection',
     function (React\Socket\ConnectionInterface $conn) use ($file, $logger) {
-        $logger->info('Writing ' . $file);
+        $logger->info('Writing {file}.', ['file' => $file]);
         $conn->write(file_get_contents($file));
     }
 );
