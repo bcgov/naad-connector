404adbc949cd32553f8b85638d2775c203c70411
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-02-27 14:40:47 -0800

    adds debugging info to the entrypoint script - will use this to rebuild the image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# display the current env
+printenv
+
+# display the current contents of /vault/secrets/naad
+cat /vault/secrets/naad
+
+source /vault/secrets/naad
+
+
+# display the current contents of /vault/secrets/naad again
+echo 'AFTER Sourcing /vault/secrets/naad'
+
+cat /vault/secrets/naad
+
+
+
 # All environment variables will be extracted from the .env inside start.php using getenv().
 /usr/local/bin/php /app/src/start.php
