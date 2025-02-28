fef63c86bea6f6e3d4795e11fe79457f016a1412
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-02-28 09:22:42 -0800

    updates entrypoint script to confirm environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # grab our secrets from the vault
-source /vault/secrets/naad
-
-# display the current contents of /vault/secrets/naad before updating
-echo 'Sourcing /vault/secrets/naad'
+echo 'Checking for vault secrets: '
 cat /vault/secrets/naad
 
+
 # display the current contents of /vault/secrets/naad again
-echo 'AFTER Sourcing /vault/secrets/naad'
+echo 'Show current environment:'
 printenv
+echo '\n'
+printenv | grep PASSWORD
 
 # All environment variables will be extracted from the .env inside start.php using getenv().
 /usr/local/bin/php /app/src/start.php
