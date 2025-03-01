4297b054270cf86ad516c3ccf3200fa34b5dbbd6
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-02-28 19:49:50 -0800

    remove entrypoint script for database, remove configmap hack to generate entrypoint script

--- deployments/kustomize/overlays/development/entrypoint.sh
+++ deployments/kustomize/overlays/development/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-echo 'Loading database secrets...' >&2
-if [ -f /vault/secrets/naad ]; then
-  . /vault/secrets/naad
-  cat /vault/secrets/naad
-  # Map Vault vars to MariaDB expected vars
-  # export MARIADB_ROOT_PASSWORD="$VAULT_MARIADB_ROOT_PASSWORD"
-  # export MARIADB_SERVICE_PORT="$VAULT_MARIADB_SERVICE_PORT"
-  # export MARIADB_SERVICE_HOST="$VAULT_MARIADB_SERVICE_HOST"
-else
-  echo "Secrets file not found" >&2
-  exit 1
-fi
+# echo 'Loading database secrets...' >&2
+# if [ -f /vault/secrets/naad ]; then
+#   . /vault/secrets/naad
+#   cat /vault/secrets/naad
+#   # Map Vault vars to MariaDB expected vars
+#   # export MARIADB_ROOT_PASSWORD="$VAULT_MARIADB_ROOT_PASSWORD"
+#   # export MARIADB_SERVICE_PORT="$VAULT_MARIADB_SERVICE_PORT"
+#   # export MARIADB_SERVICE_HOST="$VAULT_MARIADB_SERVICE_HOST"
+# else
+#   echo "Secrets file not found" >&2
+#   exit 1
+# fi
 
-# Call the default entrypoint to handle initialization
-exec /usr/local/bin/docker-entrypoint.sh mariadbd "$@"
+# # Call the default entrypoint to handle initialization
+# exec /usr/local/bin/docker-entrypoint.sh mariadbd "$@"
