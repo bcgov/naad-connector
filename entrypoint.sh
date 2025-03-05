de94c9074e3387eb50c04ea078025a243291c496
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-03-05 12:27:17 -0800

    restore app image to main branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
-# All environment variables will be extracted from the .env inside start.php using getenv().
+#!/bin/bash
 
-#!/bin/sh
-echo "Loading vault secrets for naad-app"
-while [ ! -f /vault/secrets/naad ]; do
-  echo "Waiting for /vault/secrets/naad to be available..."
-  sleep 2
-done
-echo "Vault secrets are available."
-. /vault/secrets/naad && /usr/local/bin/php /app/src/start.php
+# All environment variables will be extracted from the .env inside start.php using getenv().
+/usr/local/bin/php /app/src/start.php
