0b15f096bd7a9c4357b3d35840f9e5f68988e313
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-03-05 09:39:28 -0800

    update starting script and refine deployments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
-#!/bin/sh
-echo 'Loading naad-app secrets'
-if [ -f /vault/secrets/naad ]; then
-  . /vault/secrets/naad
-  cat /vault/secrets/naad
-  # Map Vault vars to MariaDB expected vars
-  # export MARIADB_ROOT_PASSWORD="$VAULT_MARIADB_ROOT_PASSWORD"
-  # export MARIADB_SERVICE_PORT="$VAULT_MARIADB_SERVICE_PORT"
-  # export MARIADB_SERVICE_HOST="$VAULT_MARIADB_SERVICE_HOST"
-else
-  echo "Secrets file not found" >&2
-  exit 1
-fi
-
 # All environment variables will be extracted from the .env inside start.php using getenv().
-/usr/local/bin/php /app/src/start.php
+
+#!/bin/sh
+echo "Loading vault secrets for naad-app"
+while [ ! -f /vault/secrets/naad ]; do
+  echo "Waiting for /vault/secrets/naad to be available..."
+  sleep 2
+done
+echo "Vault secrets are available."
+. /vault/secrets/naad && /usr/local/bin/php /app/src/start.php
