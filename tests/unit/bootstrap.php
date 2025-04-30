06bdb124eabce307033e22c067d7bdf4780fe29c
robrien-bcgov <richard.obrien@gov.bc.ca>
2025-04-30 15:56:16 +0000

    clear stale environment variables belonging to naad-connector

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,15 +1,43 @@
+<?php
 /**
  * Bootstrap file for unit tests.
  *
  * This script performs the following tasks:
  * - Loads the Composer autoloader to include dependencies.
- * - Clears all environment variables from the $_ENV superglobal to ensure
+ * - Clears stale environment variables from the $_ENV superglobal to ensure
  *   a clean testing environment.
- *
  */
-<?php
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-foreach (array_keys($_ENV) as $key) {
-    unset($_ENV[$key]);
-}
+// List of environment variables to unset
+$env_keys = [
+    'MARIADB_ROOT_PASSWORD',
+    'MARIADB_SERVICE_HOST',
+    'MARIADB_SERVICE_PORT',
+    'MARIADB_DATABASE',
+    'PHPMYADMIN_PORT',
+    'PMA_HOST',
+    'PMA_PORT',
+    'PMA_USER',
+    'PMA_PASSWORD',
+    'DESTINATION_URL',
+    'DESTINATION_USER',
+    'DESTINATION_PASSWORD',
+    'NAAD_PORT',
+    'NAAD_URL',
+    'LOG_LEVEL',
+    'ALERTS_TO_KEEP',
+    'LOG_PATH',
+    'LOG_RETENTION_DAYS',
+];
+
+// Unset the environment variables and log their status
+array_walk($env_keys, function ($key) {
+    if (isset($_ENV[$key])) {
+        error_log("Unsetting stale environment variable: $key");
+        unset($_ENV[$key]);
+    } else {
+        error_log("Environment variable not found: $key");
+    }
+});
